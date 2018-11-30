aa0c2f566d7e8bc48173932b8ac21bf5dbacdb8a
Michael Kelly <L00137447@student.lyit.ie>
2018-11-30 20:05:14 +0000

    Changes made before emun variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float pi = 3.14159265f;
+
+            double anotherPi = 3.141592653;
+
+            decimal number = 1.495m;
+
+            bool itWorked = true;
+
+            itWorked = false;
+
+            string message = "Hello World";
+
+            message = "nonesense yo";
+
+            char myInitial = 'm';
+            
+
+            Console.WriteLine($"Content of myInitial is {myInitial}");
+           
+            Console.ReadKey();
+
+
+     
+    }
+}
