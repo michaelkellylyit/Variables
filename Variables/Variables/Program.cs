ac9d7aab1ca0c62a5df84133b27bde4547f51887
Michael Kelly <L00137447@student.lyit.ie>
2018-12-01 15:16:31 +0000

    Added array list and list program code.

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,30 +9,108 @@ namespace Variables
 {
     class Program
     {
+
+        enum Orientation : int
+        {
+            north = 1,
+            south = 2,
+            east = 3,
+            west = 4
+        }
         static void Main(string[] args)
         {
-            float pi = 3.14159265f;
+            List<Person> people = new List<Person>();
+            people.Add(new Person {
+                FirstName = "Michael",
+                SurName = "Kelly",
+                HouseNo =4,
+                Street = "deadstreet",
+                Town ="notown",
+                County = "orange" });
+            people.Add(new Person {
+                FirstName = "Hugh",
+                SurName = "Deeney",
+                HouseNo = 5,
+                Street = "alivestreet",
+                Town = "notown",
+                County = "orange" });
 
-            double anotherPi = 3.141592653;
-
-            decimal number = 1.495m;
-
-            bool itWorked = true;
-
-            itWorked = false;
-
-            string message = "Hello World";
-
-            message = "nonesense yo";
-
-            char myInitial = 'm';
             
+            List<Person> secondList = new List<Person>();
+            secondList.Add(new Person
+            { FirstName = "Hugh",
+              SurName = "Deeney",
+              HouseNo = 5,
+              Street = "alivestreet",
+              Town = "notown",
+              County ="orange"
 
-            Console.WriteLine($"Content of myInitial is {myInitial}");
-           
+            });
+
+            people.AddRange(secondList);
+
+            //people.Remove(new Person
+            //{
+            //    FirstName = "Hugh",
+            //    SurName = "Deeney",
+            //    HouseNo = 5,
+            //    Street = "alivestreet",
+            //    Town = "notown",
+            //    County = "orange"
+
+            //});
+
+            Console.WriteLine(people.Contains(new Person
+            {
+                FirstName = "Hugh",
+                SurName = "Deeney",
+                HouseNo = 5,
+                Street = "alivestreet",
+                Town = "notown",
+                County = "orange"
+
+            }));
+
+            people.RemoveAll(f => f.SurName =="Kelly");
+
+            Console.WriteLine("Original order :");
+
+            foreach (var Person in people)
+            {
+                Console.WriteLine($"FirstName = {Person.FirstName}");
+                Console.WriteLine($"SurName = {Person.SurName}");
+                Console.WriteLine($"HouseNo = {Person.HouseNo}");
+                Console.WriteLine($"Street = {Person.Street}");
+                Console.WriteLine($"Town = {Person.Town}");
+                Console.WriteLine($"County = {Person.County}");
+            }
+
+            people.Reverse();
+
+            Console.WriteLine("Reserve order :");
+            foreach (var Person in people)
+            {
+                Console.WriteLine($"FirstName = {Person.FirstName}");
+                Console.WriteLine($"SurName = {Person.SurName}");
+                Console.WriteLine($"HouseNo = {Person.HouseNo}");
+                Console.WriteLine($"Street = {Person.Street}");
+                Console.WriteLine($"Town = {Person.Town}");
+                Console.WriteLine($"County = {Person.County}");
+            }
+
             Console.ReadKey();
+        }
+        
 
+        struct Person
+        {
+            public string FirstName;
+            public string SurName;
+            public string Street;
+            public string Town;
+            public string County;
+            public int HouseNo;
 
-     
+        }
     }
 }
